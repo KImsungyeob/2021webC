d0453f46e688c164d21dad2d18c250207c2b4f85
hostsy99 <74343264+KImsungyeob@users.noreply.github.com>
2021-05-18 12:51:11 +0900

    파이썬 GET

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/a')
-def hello():
-    return 'Hello, World!'
-
-@app.route('/b')
-def hello2():
-    return 'Hello, World!'
+@app.route('/')
+def taxi():
+    return render_template('taxi.html')
 
 @app.route('/naver')
 def naver():
-    return '안녕 나는 네이버야~'
+    return render_template('naver.html')
 
-@app.route('/taxi')
-def taxi():
-    return '''<!DOCTYPE html>
-<html>
-<body>
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
-<h2>HTML Image</h2>
-<img src="https://img2.sbs.co.kr/img/sbs_cms/WE/2021/04/05/nEn1617581537319.jpg">
+@app.route('/login')
+def login():
+    return render_template('longin.html')
 
-</body>
-</html>
+@app.route('/action_page' , methods=['GET', 'POST'])
+def action_page():
+    if request.method == 'GET':
+        return '나는 액션 GET 페이지야'
+    else:
+        search = request.form['search']
+        return '''당신은 '{}'로 검색을 했습니다.<br>
+        결과를 보여드리겠습니다. 잠시만 기다려주세요.<br>
+        리스트 ~ 
+        '''.format(search)
+    
 
 
-
-
-    '''
 
 # 웹 브라우저에 http://127.0.0.1:5000/naver
 # 위와같이 접속 하면 안녕 나는 네이버야~
